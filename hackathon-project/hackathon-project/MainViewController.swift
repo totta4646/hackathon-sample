9043e2ee51dd5fccdbd4e4e3b023037c97bd8f3a
Kohei Totani <syshayr_soccer_46@yahoo.co.jp>
2016-05-03 20:15:43 +0900

    現在地取得のロジック修正

--- hackathon-project/hackathon-project/MainViewController.swift
+++ hackathon-project/hackathon-project/MainViewController.swift
@@ -15,23 +15,17 @@ class MainViewController: BaseViewController, UIImagePickerControllerDelegate, U
     
     @IBOutlet weak var mapView: MKMapView!
     let distance : CLLocationDistance = 100
+    let clLocationManager: CLLocationManager! = CLLocationManager()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "いちなび"
     
-        var clLocationManager: CLLocationManager!
-        clLocationManager = CLLocationManager()
         clLocationManager.delegate = self
         clLocationManager.distanceFilter = 100.0
         clLocationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
-        let status = CLLocationManager.authorizationStatus()
-        
-        if(status == CLAuthorizationStatus.NotDetermined) {
-            if #available(iOS 8.0, *) {
-                clLocationManager.requestAlwaysAuthorization()
-            };
-        }
+        clLocationManager?.requestWhenInUseAuthorization()
         clLocationManager.startUpdatingLocation()
         
         let defaultLatitude: CLLocationDegrees = 37.506804
@@ -49,11 +43,9 @@ class MainViewController: BaseViewController, UIImagePickerControllerDelegate, U
         
     }
     
-    func locationManagerFunc(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
 
-        let locations: NSArray = locations as NSArray
-        let lastLocation: CLLocation = locations.lastObject as! CLLocation
-        let location:CLLocationCoordinate2D = lastLocation.coordinate
+        let location: CLLocationCoordinate2D = newLocation.coordinate
         let region: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(location, distance, distance);
 
         mapView.setRegion(region, animated: true)
@@ -63,7 +55,7 @@ class MainViewController: BaseViewController, UIImagePickerControllerDelegate, U
         let pickedImage:UIImage = info["UIImagePickerControllerOriginalImage"] as! UIImage
         let fileManager = NSFileManager.defaultManager()
         // TODO: 一旦ローカル書き込みで回避
-        let filePath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String + "/pickedimage.jpg"
+        let filePath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String + "/pickedimage.JPG"
         let imageData = UIImageJPEGRepresentation(pickedImage, 1.0)
         fileManager.createFileAtPath(filePath, contents: imageData, attributes: nil)
         
