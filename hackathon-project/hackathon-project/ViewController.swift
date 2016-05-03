4540b85bffa2f1d3321bbb34e5d98aff880fc009
Kohei Totani <syshayr_soccer_46@yahoo.co.jp>
2016-05-03 11:39:07 +0900

    first commit.

---
+++ hackathon-project/hackathon-project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hackathon-project
+//
+//  Created by Kohei Totani on 2016/05/03.
+//  Copyright © 2016年 Kohei Totani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
